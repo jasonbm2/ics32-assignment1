35ae0842fffcf1b83e65931b48ece2b05c69efba
jasonbm2 <jasonbradymartin@gmail.com>
2024-01-21 19:38:42 -0800

    giving functions to letter inputs of args

--- a1p1.py
+++ a1p1.py
@@ -1,13 +1,26 @@
 import argparse
 
+def list_directory(path, recure=False, files_only=False, search_name=None, extension=None):
+    pass
+
 def create_parser():
-    parser = argparse.ArgumentParser
+    parser = argparse.ArgumentParser()
     parser.add_argument("command", choices=["L", "Q"])
     parser.add_argument("initial_path")
-    parser.add_argument("-r", "--recure")
-    parser.add_argument("-f", "--files")
+    parser.add_argument("-r", "--recure", action="store_true")
+    parser.add_argument("-f", "--files", action="store_true")
     parser.add_argument("-s", "--search")
     parser.add_argument("-e", "--extension")
+    try:
+        args = parser.parse_args()
+    except argparse.ArgumentError as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+
+    if args.command == "Q":
+        sys.exit(0)
+    elif args.command == "L":
+        list_directory(args.initial_path, args.recure, args.files, args.search, args.extension)
 
 def main():
     create_parser()
